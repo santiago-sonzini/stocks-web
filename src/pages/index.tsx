fa533fdf3caee7a62b40cf47bf44a15c3005b382
santiago sonzini <65967521+santiago-sonzini@users.noreply.github.com>
2024-03-06 15:48:49 -0300

    no ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,10 @@ import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { embeddedLanguageFormatting } from "prettier.config.cjs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { env } from "~/env.mjs";
 import { baseUrl } from "~/utils/constants";
+import getPredictions from "~/utils/predictions";
 
 export interface StockPrediction {
   stock: string;
@@ -18,6 +19,31 @@ export interface StockPrediction {
 const Home = ({ predictions }: { predictions: [StockPrediction] }) => {
   const router = useRouter()
   const [loading, setLoading] = useState(false)
+  const [predictionsState, setPredictions] = useState<any>([]);
+
+  useEffect(() => {
+    // Define an asynchronous function inside useEffect
+    
+    const fetchData = async () => {
+      const more = [
+        'KO', 'YPF', 'MSFT', 'TS', 'SBUX', 'MCD', "AMZN", "GOOG" ,'NFLX', 'TSLA', 'MELI', 'GLOB', ]
+      // Call your asynchronous function
+      const result = await getPredictions(more);
+      // Update state with the returned value
+      const updated = [ ...result]
+      updated.sort((a, b) => b.pct_change - a.pct_change);
+      console.log(updated);
+
+      setPredictions(updated);
+    };
+
+    // Call the fetchData function
+    fetchData();
+    console.log("predictionsState");
+    console.log(predictionsState.length);
+
+    
+  }, []); // Empty dependency array means this effect will only run once after the initial render
   return (
     <>
       <Head>
@@ -26,7 +52,11 @@ const Home = ({ predictions }: { predictions: [StockPrediction] }) => {
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📈</text></svg>"/>
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        
+        {
+          predictionsState.length > 0 && predictionsState
+          ?
+          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] flex justify-center items-center">
             <span className="text-red-500">Stock</span>
             <span className="text-green-500">s</span>
@@ -82,34 +112,15 @@ const Home = ({ predictions }: { predictions: [StockPrediction] }) => {
           </button>
           
         </div>
+        :
+        <h1 className={`" text-xl font-extrabold tracking-tight text-white  flex justify-center items-center hover:underline  animate-pulse  "`}>
+            
+              <span className={`"  " text-green-500 " "`} >{"Loading 😁"}</span>
+        </h1>
+      }
       </main>
     </>
   );
 };
 
-
-export async function getServerSideProps() {
-  const stocks = [ 'MELI',"GOOG"];
-  console.log(baseUrl);
-  
-  // Fetch data from an external API
-  const promises = stocks.flatMap(async (item) => {
-    try {
-      const res = await fetch(`${baseUrl}/prediction/${item}`);
-      return res.json();
-    } catch (error) {
-      console.log(error);
-      return [];
-    }
-  });
-
-  const predictions = await Promise.all(promises);
-
-  predictions.sort((a, b) => b.pct_change - a.pct_change);
-  //console.log(predictions);
-
-  return {
-    props: { predictions },
-  };
-}
 export default Home;
